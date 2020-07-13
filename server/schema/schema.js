400fb3ded173390f539e0dff0120614e29ad0746
gprakarsh <379@doonschool.com>
2020-07-13 16:13:54 +0530

    video 9 complete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
  
 const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+//dummy data
+
+let books = [
+    {name: 'book1', genre:'Fantasy', id:'1'},
+    {name: 'book2', genre:'Sci-Fi', id:'2'},
+    {name: 'book3', genre:'Thriller', id:'3'}
+];
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -19,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
             args: {id:{type: GraphQLString}},
             resolve(parent,args){
                 //code to get data from db / other source
+                return _.find(books, {id:args.id});
             }
         }
     }
